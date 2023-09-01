d7496e320e385296bb1aafe7145d616e651fb47d
SonGangMin <skm0628@naver.com>
2023-09-01 14:10:06 +0900

    db세팅완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App:React.FC = () => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="App">안녕하세요</div>;
+};
 
 export default App;
